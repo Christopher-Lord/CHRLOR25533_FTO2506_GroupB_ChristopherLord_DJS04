99de28a4c86e73d582b41b1cc535f2686e2289c2
Christopher-Lord <lordc42@gmail.com>
2025-11-14 12:11:34 +0200

    Add clear search button to component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,12 +3,19 @@ import "./SearchBar.css";
 export default function SearchBar({ searchTerm, setSearchTerm }) {
   return (
     <section className="search-bar">
-      <input
-        type="text"
-        placeholder="Search podcasts"
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
+      <div className="search-wrapper">
+        <input
+          type="text"
+          placeholder="Search podcasts"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        {searchTerm && (
+          <span className="clear-icon" onClick={() => setSearchTerm("")}>
+            ✖
+          </span>
+        )}
+      </div>
     </section>
   );
 }
